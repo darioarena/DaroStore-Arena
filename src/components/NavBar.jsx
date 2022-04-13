bf5f3ad30ee28d3be001ffa481ad12709399c644
darioarena <darioarenacba@gmail.com>
2022-04-13 15:18:40 -0300

    main

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import './NavBar.css';
+import imgLogo from '../assets/img/logo.png';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+
+import { loadAnimation } from "lottie-web";
+import { defineLordIconElement } from "lord-icon-element";
+defineLordIconElement(loadAnimation);
+
+function NavBar(props) {
+  return (
+    <div className='navbar'>
+      <section className='identidad'>
+      <img src={imgLogo} alt="Logo Ecommerce" className='logo'/>
+      <h2 className='titulo'>DaroStore</h2>
+      </section>
+      <ul className='menu'>
+        <li><a href="a" className='opcion'>PCs</a></li>
+        <li><a href="b" className='opcion'>Perifericos</a></li>
+        <li><a href="c" className='opcion'>Mobiliario</a></li>
+        {/* <li><FontAwesomeIcon icon="fa-solid fa-cart-shopping" className='opcion' id='carrito'/></li> */}
+        <li><lord-icon trigger="hover" src="https://cdn.lordicon.com/slkvcfos.json" className='opcion' id='carrito'></lord-icon></li>
+      </ul>
+    </div>
+  );
+}
+
+export default NavBar;
